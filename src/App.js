cc59f0a69ec43184b86d6b64eb72b0128ad59105
Yamini Rathish <36505387+yaminirathish11@users.noreply.github.com>
2023-03-23 18:57:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, createContext } from "react";
+import Component1 from "./components/component1";
+
+const NameContext = createContext();
 
 function App() {
+  const [name, setName] = useState("RatYam");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NameContext.Provider value={name}>
+        <h1>Hello world app</h1>
+        <Component1 />
+      </NameContext.Provider>
     </div>
   );
 }
 
-export default App;
+export {App, NameContext};
+
+// Init - const variableName = createContext();
+// Value Provide - 
+{/* <variableName.Provider value={name}>
+<h1>Hello world app</h1>
+<Component1 />
+</variableName.Provider> */}
+
+//useContext - to extract the value
